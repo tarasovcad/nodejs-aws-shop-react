d576e991746e1c9a74ff8319d20aa5f7b38a502d
tarasovcad <tarasovcad@gmail.com>
2025-02-16 00:49:55 -0600

    feat: create S3 bucket

--- cdk/lib/my-cdk-app-stack.ts
+++ cdk/lib/my-cdk-app-stack.ts
@@ -1,16 +1,29 @@
 import * as cdk from 'aws-cdk-lib';
+import { RemovalPolicy } from 'aws-cdk-lib';
+import { Bucket, BlockPublicAccess } from 'aws-cdk-lib/aws-s3';
+import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class MyCdkAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    //  create a bucket
+    const websiteBucket = new Bucket(this, 'ShopStaticBucket', {
+      bucketName: 'aws-react-rs-school',
+      websiteIndexDocument: 'index.html',
+      publicReadAccess: true,
+      blockPublicAccess: BlockPublicAccess.BLOCK_ACLS,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'MyCdkAppQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new BucketDeployment(this, 'DeployReactApp', {
+      sources: [Source.asset('../dist')],
+      destinationBucket: websiteBucket,
+    });
+
+    new cdk.CfnOutput(this, 'BucketWebsiteURL', {
+      value: websiteBucket.bucketWebsiteUrl,
+      description: 'The URL for the deployed React app',
+    });
   }
 }
