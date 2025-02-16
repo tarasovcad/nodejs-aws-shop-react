b0abcd22c1dc601764f1fd4e7914d1689affa3b8
tarasovcad <tarasovcad@gmail.com>
2025-02-16 01:31:04 -0600

    feat: create a cloudfront distribution

--- cdk/lib/my-cdk-app-stack.ts
+++ cdk/lib/my-cdk-app-stack.ts
@@ -1,9 +1,10 @@
 import * as cdk from 'aws-cdk-lib';
-import { RemovalPolicy } from 'aws-cdk-lib';
 import { Bucket, BlockPublicAccess } from 'aws-cdk-lib/aws-s3';
 import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
-import { Construct } from 'constructs';
+import { Distribution, ViewerProtocolPolicy } from 'aws-cdk-lib/aws-cloudfront';
+import { S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
 
+import { Construct } from 'constructs';
 export class MyCdkAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
@@ -11,19 +12,33 @@ export class MyCdkAppStack extends cdk.Stack {
     //  create a bucket
     const websiteBucket = new Bucket(this, 'ShopStaticBucket', {
       bucketName: 'aws-react-rs-school',
-      websiteIndexDocument: 'index.html',
-      publicReadAccess: true,
+      publicReadAccess: false,
       blockPublicAccess: BlockPublicAccess.BLOCK_ACLS,
     });
 
+    //  create a cloudfront distribution
+    const distribution = new Distribution(this, 'SiteDistribution', {
+      defaultBehavior: {
+        origin: new S3Origin(websiteBucket),
+        viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
+    });
+
+    //  create a deployment
     new BucketDeployment(this, 'DeployReactApp', {
       sources: [Source.asset('../dist')],
       destinationBucket: websiteBucket,
+      distribution: distribution,
+      distributionPaths: ['/*'],
     });
-
+    //  output the website url
     new cdk.CfnOutput(this, 'BucketWebsiteURL', {
       value: websiteBucket.bucketWebsiteUrl,
       description: 'The URL for the deployed React app',
+    });
+    new cdk.CfnOutput(this, 'CloudFrontURL', {
+      value: distribution.domainName,
+      description: 'The URL for the CloudFront distribution',
     });
   }
 }
